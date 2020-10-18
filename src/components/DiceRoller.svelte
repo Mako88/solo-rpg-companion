810516270514593b64f598816309dae9e2ae5ab1
Mako88 <afuturepilotis@gmail.com>
2020-10-18 02:11:38 -0500

    Add <Template> tag to DiceRoller

--- src/components/DiceRoller.svelte
+++ src/components/DiceRoller.svelte
@@ -48,23 +48,25 @@
   }
 </style>
 
-<div class="tile is-1 is-ancester roller" bind:this={roller}>
-  <div class="tile is-vertical is-parent">
-    <div class="tile is-child handle" bind:this={handle}>
-      <i class="fas fa-grip-lines fa-5x primary" />
+<template>
+  <div class="tile is-1 is-ancester roller" bind:this={roller}>
+    <div class="tile is-vertical is-parent">
+      <div class="tile is-child handle" bind:this={handle}>
+        <i class="fas fa-grip-lines fa-5x primary" />
+      </div>
+      <img class="tile is-child" src="images/d4.png" alt="d4" />
     </div>
-    <img class="tile is-child" src="images/d4.png" alt="d4" />
   </div>
-</div>
-<Moveable
-  dragTarget={handle}
-  target={roller}
-  draggable={true}
-  throttleDrag={0}
-  on:dragStart={({ detail: { set } }) => {
-    set(frame.translate);
-  }}
-  on:drag={({ detail: { target, beforeTranslate } }) => {
-    frame.translate = beforeTranslate;
-    target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`;
-  }} />
+  <Moveable
+    dragTarget={handle}
+    target={roller}
+    draggable={true}
+    throttleDrag={0}
+    on:dragStart={({ detail: { set } }) => {
+      set(frame.translate);
+    }}
+    on:drag={({ detail: { target, beforeTranslate } }) => {
+      frame.translate = beforeTranslate;
+      target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`;
+    }} />
+</template>
